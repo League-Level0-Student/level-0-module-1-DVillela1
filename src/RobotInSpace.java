8dafb4b4ad7ec0fb37e6129c566b69747b7c8a64
league <league@worthymini-6.attlocal.net>
2017-11-22 11:30:18 -0800

    RobotInSpace SecretMessageBox

--- src/RobotInSpace.java
+++ src/RobotInSpace.java
@@ -16,26 +16,64 @@
 	
 	public class RobotInSpace implements KeyEventDispatcher {
 		
-	Robot rob = new Robot("mini");
+	Robot bot = new Robot("mini");
 
 	/*  Make the Robot move around the screen when the arrow keys are pressed... */
 	private void moveRobot(int keyPressed) {
 	    // 0. Print out the keyPressed variable and write down the numbers for each arrow key
-
+		System.out.println(keyPressed);
 	    // 1. If the up arrow is pressed, move the Robot up the screen.
-
+		if(38 == keyPressed) {
+			try {
+				bot.microMove(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
 	    // 2. If the down arrow is pressed, move the Robot down.
-
+		if(40 == keyPressed) {
+			try {
+				bot.microMove(-5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 	    // 3. If the left arrow is pressed, make the Robot go left. Hint: Make sure to end with the Robot facing UP.
-	    
+	    if(37 == keyPressed) {
+			try {
+				bot.setAngle(-90);
+				bot.microMove(5);
+				bot.setAngle(0);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	    // 4. If right is pressed, move the Robot right.
+	    if(39 == keyPressed) {
+			try {
+				bot.setAngle(90);
+				bot.microMove(5);
+				bot.setAngle(0);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	    
 	    
 	    // 5. Then move the Robot to RD-2D for a surprise! 
 	}
 
 	private void checkIfR2D2Found() throws Exception {
-	    int robotLocationX = rob.getX();
-	    int robotLocationY = rob.getY();
+	    int robotLocationX = bot.getX();
+	    int robotLocationY = bot.getY();
 
 		if (robotLocationX <= 7300 && robotLocationX >= 720 && robotLocationY >= 150 && robotLocationY <= 160)
 	        playEureka();
@@ -48,9 +86,9 @@
 
 	private void controlTheRobot() {
 	    KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
-	    rob.setWindowImage("planet.jpg");
-	    rob.penUp();
-	    rob.setSpeed(10);
+	    bot.setWindowImage("planet.jpg");
+	    bot.penUp();
+	    bot.setSpeed(10);
 	}
 
 	public boolean dispatchKeyEvent(KeyEvent e) {
