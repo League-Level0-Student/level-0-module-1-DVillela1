ba6f4c4818d26d85fb7ee36734916a624d7f7ccc
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2017-12-06 19:16:18 -0800

    MyAges, Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,20 @@
+
+public class Badgers {
+public static void main(String[] args) {
+for (int i = 1; i < 4; i++) {
+for (int j = 1; j < 11; j++) {
+System.out.print("Badger, ");
+}
+System.out.println(" ");
+for (int j = 1; j < 3; j++) {
+System.out.print("Mushroom, ");	
+}	
+System.out.println("a");
+}	
+}
+	
+	
+	
+	
+}
+
---
+++ src/MyAges.java
@@ -0,0 +1,12 @@
+
+public class MyAges {
+public static void main(String[] args) {
+for (int i = 0; i < 13; i++) {
+	System.out.println(i);
+}
+	
+	
+	
+	
+}
+}
